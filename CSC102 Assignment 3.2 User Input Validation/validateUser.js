5680a3d5f33b34a240e2515bf34a1a2fd0676425
trophimus <akilbonner@gmail.com>
2023-12-04 03:26:15 -0600

    Add files via upload

---
+++ CSC102 Assignment 3.2 User Input Validation/validateUser.js
@@ -0,0 +1,38 @@
+ // var firstName = document.getElementById('firstName').value;
+function validateInput() {
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let zipCode = document.getElementById('zipCode').value;
+    let fullName = firstName + " " + lastName;
+
+    if (fullName.length > 20) {
+        alert("Name must be less than 20 characters.");
+        return;
+    }
+
+    if (!/^\d{5}$/.test(zipCode)) {
+        alert("Zip code must be 5 digits.");
+        return;
+    }
+    // alert("Welcome to the website!");
+    document.getElementById('message').innerText = "Secret Message: Welcome to the website!";
+}
+
+
+/*var c = document.getElementById("agentCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke(); */
+
+window.onload = function () {
+    var audio = document.getElementById('myAudio');
+    // Check if autoplay is allowed
+    audio.play().then(() => {
+        console.log("Audio playback started automatically");
+    }).catch(error => {
+        console.log("Audio playback was prevented. Error: ", error);
+        // error is a DOMException object
+    });
+};
+
